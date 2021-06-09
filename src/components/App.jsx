cac6a84ff8c389c7bf1e9a0a98a10edb618cb63e
cody-mckeon <codymckeon@gmail.com>
2021-06-09 13:40:52 -0700

    Map isn't working properly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleClick() {
+    setItems((prevItems) => {
+      return [...prevItems, item];
+    });
+    setItems("");
+  }
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setItem(newValue);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" value={item} onChange={handleChange} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((todoItem) => {
+            return <li>{todoItem}</li>;
+          })}
         </ul>
       </div>
     </div>
